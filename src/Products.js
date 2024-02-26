8740b88b147a1754d3af5aee9bfb8b7f9190ae3f
Argha Mallick <arghamallick5@gmail.com>
2024-02-26 22:54:47 +0530

    styled components setup

---
+++ src/Products.js
@@ -0,0 +1,30 @@
+// import React from "react";
+// import styled from "styled-components";
+
+// const Products = () => {
+//   return <div></div>;
+// };
+
+// const Wrapper = styled.section`
+//   .grid-filter-column {
+//     grid-template-columns: 0.2fr 1fr;
+//   }
+
+//   @media (max-width: ${({ theme }) => theme.media.mobile}) {
+//     .grid-filter-column {
+//       grid-template-columns: 1fr;
+//     }
+//   }
+// `;
+
+// export default Products;
+
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
