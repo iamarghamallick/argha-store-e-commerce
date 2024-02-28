ea683678cdefd8afcf1537931e43727691a1b641
Argha Mallick <arghamallick5@gmail.com>
2024-02-28 20:42:57 +0530

    product image

--- src/components/PageNavigation.js
+++ src/components/PageNavigation.js
@@ -5,13 +5,15 @@ import styled from 'styled-components'
 const PageNavigation = ({ category, title }) => {
     return (
         <Wrapper>
-            <h3><NavLink to='/'>Home</NavLink> / {category} / {title}</h3>
+            <h3 className='navigation-path'><NavLink to='/'>Home</NavLink> / {category} / {title}</h3>
         </Wrapper>
     )
 }
 
 const Wrapper = styled.section`
-
+    .navigation-path {
+        margin: 1rem;
+    }
 `
 
 export default PageNavigation
