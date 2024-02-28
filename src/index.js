7b73f4f4e17e8653c67364018a9dc53967788001
Argha Mallick <arghamallick5@gmail.com>
2024-02-28 23:27:00 +0530

    products page

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppProvider } from './context/productContext';
+import { FilterContextProvider } from './context/filterContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <App />
+      <FilterContextProvider>
+        <App />
+      </FilterContextProvider>
     </AppProvider>
   </React.StrictMode>
 );
